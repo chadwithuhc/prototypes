3817dcaa9ce99790b4e816ed6a091f3378d46147
chadwithuhc <chadwithuhc@gmail.com>
2017-10-21 15:39:39 -0600

    Are you sure?

---
+++ are-you-sure/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <fieldset className="button-revealer">
+          <button name="submit" className="button-revealer__button">Delete</button>
+          <label htmlFor="submit" className="button-revealer__label">Are you sure?</label>
+        </fieldset>
+
+        <fieldset className="button-revealer button-revealer--grow">
+          <button name="submit" className="button-revealer__button">Delete</button>
+          <label htmlFor="submit" className="button-revealer__label">Are you sure?</label>
+        </fieldset>
+
+        <fieldset className="button-revealer button-revealer--verbose hidden">
+          <button name="submit" className="button-revealer__button"><span className="button-revealer__verbose-text">Yes</span><span className="button-revealer__verbose-text-start">Delete</span></button>
+          <label htmlFor="submit" className="button-revealer__label">Are you sure?</label>
+        </fieldset>
+      </main>
+    );
+  }
+}
+
+export default App;
